48a893dd1d237bda34c5167a23839702162b06e2
Faiyaz Khira <faiyazkhira27@gmail.com>
2025-06-26 11:45:13 +0530

    Initial commit - Vite + React + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import DashboardPage from "./pages/DashboardPage"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App;
